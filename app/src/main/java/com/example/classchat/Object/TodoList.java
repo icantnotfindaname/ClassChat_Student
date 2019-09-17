7538dce21a1a627c4d03bf2407760aecca0035ab
SingXuY <201830570361@mail.scut.edu.cn>
2019-09-17 17:04:43 +0800

    添加了  添加待办的Acivity~

---
+++ app/src/main/java/com/example/classchat/Object/TodoList.java
@@ -0,0 +1,87 @@
+package com.example.classchat.Object;
+
+import com.googlecode.mp4parser.srt.SrtParser;
+
+public class TodoList{
+
+    private String id;
+    private String title;
+    private int classtime;
+    private String todotime;
+    private String todoplace;
+    private String details;
+    private Boolean isClock;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getClasstime() {
+        return classtime;
+    }
+
+    public void setClasstime(int classtime) {
+        this.classtime = classtime;
+    }
+
+    public String getTodotime() {
+        return todotime;
+    }
+
+    public void setTodotime(String todotime) {
+        this.todotime = todotime;
+    }
+
+    public String getTodoplace() {
+        return todotime;
+    }
+
+    public void setTodoplace(String todoplace) {
+        this.todoplace = todoplace;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public Boolean getClock() {
+        return isClock;
+    }
+
+    public void setClock(Boolean clock) {
+        isClock = clock;
+    }
+
+
+    public TodoList(){
+        // TODO Auto-generated constructor stub
+    }
+
+    public TodoList(String id,String title,int classtime,String todotime,String todoplace,String details,boolean isClock){
+        this.id = id;
+        this.title = title;
+        this.classtime = classtime;
+        this.todotime=todotime;
+        this.todoplace=todoplace;
+        this.details=details;
+        this.isClock=isClock;
+    }
+
+}
