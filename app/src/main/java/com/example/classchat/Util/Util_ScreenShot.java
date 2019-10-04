d61ffc29b3ef766e073b93cb70beb88313afebe9
kevin <1612365371@qq.com>
2019-10-04 23:04:33 +0800

    帮助与反馈优化

--- app/src/main/java/com/example/classchat/Util/Util_ScreenShot.java
+++ app/src/main/java/com/example/classchat/Util/Util_ScreenShot.java
@@ -1,18 +1,26 @@
 package com.example.classchat.Util;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Rect;
 import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class Util_ScreenShot {
+
+    private static final String SAVE_PIC_PATH= Environment.getExternalStorageDirectory().getAbsolutePath();
+
     private static Bitmap takeScreenShot(Activity activity) {
         // View是你需要截图的View
         View view = activity.getWindow().getDecorView();
@@ -37,33 +45,45 @@ public class Util_ScreenShot {
         return b;
     }
 
-    // 保存到sdcard
-    private static void savePic(Bitmap b, String strFileName) {
+    private static void saveFile(Bitmap bm, String fileName, Context context) throws IOException {
+        String subForder = SAVE_PIC_PATH;
+        Log.e("SAVE_PIC_PATH", SAVE_PIC_PATH);
+        File foder = new File(subForder);
+        if (!foder.exists()) {
+            foder.mkdirs();
+        }
 
-        FileOutputStream fos = null;
+        File myCaptureFile = new File(subForder, fileName);
+        if (!myCaptureFile.exists()) {
+            myCaptureFile.createNewFile();
+        }
+
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(myCaptureFile));
+        bm.compress(Bitmap.CompressFormat.JPEG, 100, bos);
+        bos.flush();
+        bos.close();
+
         try {
-            fos = new FileOutputStream(strFileName);
-            if (null != fos) {
-                b.compress(Bitmap.CompressFormat.PNG, 90, fos);
-                fos.flush();
-                fos.close();
-//                Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-//                Uri uri = Uri.fromFile(file);
-//                intent.setData(uri);
-//                context.sendBroadcast(intent);
-            }
+            MediaStore.Images.Media.insertImage(context.getContentResolver(),
+                    myCaptureFile.getAbsolutePath(), fileName, null);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
+
+
+        //通知图库更新
+        Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        Uri uri = Uri.fromFile(myCaptureFile);
+        Log.e("Uri", String.valueOf(uri));
+        intent.setData(uri);
+        context.sendBroadcast(intent);
 
     }
 
     // 程序入口
-    public static String shoot(Activity activity) {
-        String strFileName = "sdcard/" + System.currentTimeMillis() + ".jpg";
-        Util_ScreenShot.savePic(Util_ScreenShot.takeScreenShot(activity), strFileName);
+    public static String shoot(Activity activity) throws IOException {
+        String strFileName = "Laoke" + System.currentTimeMillis() + ".jpg";
+        Util_ScreenShot.saveFile(Util_ScreenShot.takeScreenShot(activity), strFileName, activity);
         return strFileName;
     }
 }
