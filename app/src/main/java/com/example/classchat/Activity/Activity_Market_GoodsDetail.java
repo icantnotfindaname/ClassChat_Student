9cc2751a4bd1211721c9b41b8f418f3080f01ffd
kevin <1612365371@qq.com>
2019-10-19 01:14:58 +0800

    对比课表改进

--- app/src/main/java/com/example/classchat/Activity/Activity_Market_GoodsDetail.java
+++ app/src/main/java/com/example/classchat/Activity/Activity_Market_GoodsDetail.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
@@ -15,12 +16,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewTreeObserver;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -66,6 +71,8 @@ public class Activity_Market_GoodsDetail extends AppCompatActivity{
     private ConvenientBanner mBanner;
     private RecyclerView rv;
     private Adapter_GoodsDetail myAdapter;
+    private PopupWindow popupWindow;
+    private RecyclerView param1, param2, param3;
 
 
     //handler处理反应回来的信息
@@ -89,6 +96,7 @@ public class Activity_Market_GoodsDetail extends AppCompatActivity{
         name.setText(object_item_detail.getItem().getName());
 
         mBanner = findViewById(R.id.banner);
+        getDetailFromWeb();
         mBanner.setPages(new CBViewHolderCreator<NetworkImageHolderView>() {
             @Override
             public NetworkImageHolderView createHolder() {
@@ -107,6 +115,10 @@ public class Activity_Market_GoodsDetail extends AppCompatActivity{
         rv.setAdapter(myAdapter);
     }
 
+    private void getDetailFromWeb() {
+        //TODO
+    }
+
     public void back(View view) {
         finish();
     }
@@ -122,6 +134,40 @@ public class Activity_Market_GoodsDetail extends AppCompatActivity{
 
     public void buy(View view) {
         //TODO 购买逻辑
+    }
+
+    public void choose(View view) {
+        View inflate = LayoutInflater.from(Activity_Market_GoodsDetail.this).inflate(R.layout.item_choose_goods_popupwindow, null);
+        popupWindow = new PopupWindow(inflate, RelativeLayout.LayoutParams.MATCH_PARENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT);
+        //设置背景,这个没什么效果，不添加会报错
+        popupWindow.setBackgroundDrawable(new ColorDrawable(0x292421));
+        //设置点击弹窗外隐藏自身
+        popupWindow.setAnimationStyle(R.style.Animation_Design_BottomSheetDialog);
+        popupWindow.setFocusable(true);
+        popupWindow.setOutsideTouchable(false);
+        //设置位置
+        popupWindow.showAtLocation(inflate, Gravity.BOTTOM, 0, 0);
+        //设置PopupWindow的View点击事件
+        setOnPopupViewClick(inflate);
+    }
+
+    private void setOnPopupViewClick(View view) {
+        Button close = view.findViewById(R.id.close_choose);
+        RecyclerView param1 = view.findViewById(R.id.param_1);
+        RecyclerView param2 = view.findViewById(R.id.param_2);
+        RecyclerView param3 = view.findViewById(R.id.param_3);
+
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popupWindow.dismiss();
+            }
+        });
+
+        for(int i = 0; i < object_item_detail.getParamList().size(); ++i){
+
+        }
     }
 }
 
--- app/src/main/java/com/example/classchat/Adapter/Adapter_Goods_Detail_Choose.java
+++ app/src/main/java/com/example/classchat/Adapter/Adapter_Goods_Detail_Choose.java
@@ -6,52 +6,49 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.ImageView;
+import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
 import com.example.classchat.R;
 
 import java.util.List;
 
-public class Adapter_GoodsDetail extends RecyclerView.Adapter<Adapter_GoodsDetail.ViewHolder> {
+public class Adapter_Goods_Detail_Choose extends RecyclerView.Adapter<Adapter_Goods_Detail_Choose.ViewHolder> {
 
     private Context mContext;
-    private List<String>imageList;
+    private List<String> paramList;
 
-    public Adapter_GoodsDetail(Context context, List<String>imageList){
+    public Adapter_Goods_Detail_Choose(Context context, List<String> paramList) {
         mContext = context;
-        this.imageList = imageList;
+        this.paramList = paramList;
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        private ImageView pic;
+        private TextView param;
 
         public ViewHolder(View view) {
             super(view);
-            pic = view.findViewById(R.id.iv_adapter_good_detail_img);
+            param = view.findViewById(R.id.param);
         }
     }
 
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         View view = LayoutInflater.from(viewGroup.getContext())
-                .inflate(R.layout.adapter_good_detail_imgs, viewGroup, false);
-        Adapter_GoodsDetail.ViewHolder holder = new Adapter_GoodsDetail.ViewHolder(view);
+                .inflate(R.layout.item_goods_choose_param, viewGroup, false);
+        Adapter_Goods_Detail_Choose.ViewHolder holder = new Adapter_Goods_Detail_Choose.ViewHolder(view);
         return holder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        Glide.with(mContext).load(imageList.get(position)).into(holder.pic);
+        holder.param.setText(paramList.get(position));
     }
 
     @Override
     public int getItemCount() {
-        if(imageList !=null)
-            return imageList.size();
+        if (paramList != null)
+            return paramList.size();
         else
             return 0;
     }
-
 }
